d99473b2742f1e9822c3e9061a04518b863e8999
raju15022004 <raju032004@gmail.com>
2026-05-13 23:37:07 +0530

    index.js

---
+++ src/pages/About.js
@@ -0,0 +1,32 @@
+function Myabout()
+{
+  return <h1>this is my data</h1>
+}
+
+export default Myabout
+
+function Aboutpage(){
+  return <h3>this is heading three</h3>
+}
+function Contactus(){
+  return(
+    <h2>this is my heading two</h2>
+  )
+}
+
+export function Service(){
+
+return(
+  <div>
+    <h1>Raju</h1>
+    <p>this is my text</p>
+    <ul>
+      <li>home</li>
+      <li>home</li>
+      <li>home</li>
+      <li>home</li>
+    </ul>
+  </div>
+)
+}
+export {Aboutpage,Contactus}
