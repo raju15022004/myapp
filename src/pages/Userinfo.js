1f7bf0ce2fe0f9fa5cba05e3910239ea51e5efe2
raju15022004 <raju032004@gmail.com>
2026-05-14 15:00:22 +0530

    Userinfo.js

--- src/pages/Userinfo.js
+++ src/pages/Userinfo.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function Userinfo(){
+
+  const uname="kumar ";
+  const age="50";
+  const user={username:"raju",age:40}
   return(
     <div>
-    <h1>user name</h1>
-    <h1>user age</h1>
+    <h1>user name:{uname}</h1>
+    <h1>user age:{age}</h1>
     <h1>user email</h1>
     <h1>user phone</h1>
+    <h1>fees:{100+200+400}</h1>
     </div>
   )
 }
