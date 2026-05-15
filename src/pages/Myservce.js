892574bbf363a359f1a09a6c3529b28a06af16f7
raju15022004 <raju032004@gmail.com>
2026-05-15 14:55:44 +0530

    myservce.js

---
+++ src/pages/Myservce.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Myservce(){
+
+  const uname="kumar ";
+  const age="50";
+  const user={username:"raju",age:40}
+  return(
+    <div>
+    <h1>user name:{uname}</h1>
+    <h1>user age:{age}</h1>
+    <h1>user email</h1>
+    <h1>user phone</h1>
+    <h1>fees:{100*200*400}</h1>
+    </div>
+  )
+}
+
+export default Myservce
+
